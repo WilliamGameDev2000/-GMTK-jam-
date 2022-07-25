b83a9726b885b017c3cc6d7e7bec585634e2ef17
William <Hamish2.Lawrence@live.uwe.ac.uk>
2022-07-25 15:04:22 +0100

    simple dice roll number generator test

--- GMTK Game Jam 2022/Assets/Scripts/DiceCheckZone.cs
+++ GMTK Game Jam 2022/Assets/Scripts/DiceCheckZone.cs
@@ -4,18 +4,70 @@ using UnityEngine;
 
 public class DiceCheckZone : MonoBehaviour
 {
+    public static DiceCheckZone checkZone;
     Vector3 velocity;
+
+    private void Awake()
+    {
+        if (checkZone == null)
+        {
+            checkZone = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+        
+    }
 
     void FixedUpdate()
     {
-        velocity = DiceRoll.Dice.GetComponent<Rigidbody>().velocity;
+        velocity = DiceRoll.DiceVel;
     }
 
-    void OnTriggerEnter(Collider col)
+    void OnTriggerStay(Collider col)
     {
-        if(velocity.x == 0 && velocity.y == 0 && velocity.z ==0)
+        if (velocity.x == 0f && velocity.y == 0f && velocity.z ==0f)
         {
-
+            switch(col.gameObject.name)
+            {
+                case "side1":
+                    {
+                        Debug.Log("Side6 Has landed");
+                        break;
+                    }
+                case "side2":
+                    {
+                        Debug.Log("Side5 Was rolled");
+                        break;
+                    }
+                case "side3":
+                    {
+                        Debug.Log("Side4 Facing up");
+                        break;
+                    }
+                case "side4":
+                    {
+                        Debug.Log("Side3 Is skyward");
+                        break;
+                    }
+                case "side5":
+                    {
+                        Debug.Log("Side2 Is live");
+                        break;
+                    }
+                case "side6":
+                    {
+                        Debug.Log("Side1 Our result");
+                        break;
+                    }
+            }
+            Activate(false);
         }
+    }
+
+    public void Activate(bool activate)
+    {
+        gameObject.SetActive(activate);
     }
 }
--- GMTK Game Jam 2022/Assets/Scripts/DiceRoll.cs
+++ GMTK Game Jam 2022/Assets/Scripts/DiceRoll.cs
@@ -5,30 +5,36 @@ using UnityEngine;
 public class DiceRoll : MonoBehaviour
 {
     public static GameObject Dice;
-    Rigidbody DiceRb;
+    static Rigidbody DiceRb;
+    public static Vector3 DiceVel;
 
     // Start is called before the first frame update
     void Start()
     {
+        Dice = gameObject;
         DiceRb = Dice.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        DiceVel = DiceRb.velocity;
+
         if(Input.GetKeyDown(KeyCode.A))
         {
-            foreach(Transform side in Dice.transform)
-            {
-                side.GetComponent<SphereCollider>().enabled = true;
-            }
-            float dirX = Random.Range(0, 500);
-            float dirY = Random.Range(0, 500);
-            float dirZ = Random.Range(0, 500);
+            float dirX = Random.Range(50, 750);
+            float dirY = Random.Range(50, 750);
+            float dirZ = Random.Range(50, 750);
             transform.rotation = Quaternion.identity;
+            transform.position = new Vector3(-3.43099999f, 7.07000017f, -11.4829998f);
 
-            DiceRb.AddForce(transform.up * 350);
+            DiceRb.AddForce(transform.up * 550);
             DiceRb.AddTorque(dirX, dirY, dirZ);
+        }
+
+        if (DiceVel.x > 0f || DiceVel.y > 0f || DiceVel.z > 0)
+        {
+            DiceCheckZone.checkZone.Activate(true);
         }
     }
 }
